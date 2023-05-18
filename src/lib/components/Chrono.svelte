c9420e23c9ea2e2db593a6fc6ac04df6a4680b90
unknown <dam.fayol@gmail.com>
2023-05-18 21:41:03 +0200

    Add Chrono component

---
+++ src/lib/components/Chrono.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+
+    let time = 0;
+    let state = 'paused';
+
+    let start = () => { state = 'started'};
+    let pause = () => { state = 'paused'};
+    let clear = () => { state = 'clear'};
+
+    let interval : number | null = null;
+
+    $: {
+        if(state == 'started' && interval == null) {
+            interval = window.setInterval(() => {
+                time += 1;
+            }, 1000);
+        }
+
+        if(state == 'paused') {
+            interval && clearInterval(interval);
+            interval = null;
+        }
+
+        if(state == 'clear') {
+            time = 0;
+            interval && clearInterval(interval);
+            interval = null;
+            state = 'paused';
+        }
+    }
+
+    $: mm = Math.floor(time / 60).toString().padStart(2, '0');
+    $: ss = (time % 60).toString().padStart(2, '0');
+
+</script>
+
+<div>
+    <button on:click={start}>Start</button>
+    <button on:click={pause}>Pause</button>
+    <button on:click={clear}>Clear</button>
+    {mm}:{ss}
+</div>
