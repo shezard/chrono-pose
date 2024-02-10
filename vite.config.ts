5e320a17b1655878e7ee4bd276044b7113f8f8f0
unknown <dam.fayol@gmail.com>
2024-02-10 20:58:57 +0100

    fix: correct build target

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    build: {
+        target: "es2022"
+    },
+    esbuild: {
+        target: "es2022"
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            target: "esnext",
+        },
+    },
 });
